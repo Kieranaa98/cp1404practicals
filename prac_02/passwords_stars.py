0748cda46ff3e871d6f11b4617b254a9b547ba62
kierananthiniabraham <kieran.abraham@my.jcu.edu.au>
2025-02-08 20:47:18 +0800

    Added dot strings for password stars.

--- prac_02/passwords_stars.py
+++ prac_02/passwords_stars.py
@@ -1,17 +1,20 @@
 PASSWORD_LENGTH =10
 
 def main():
+    """Main function"""
     password = get_password()
 
     get_asterisk(password)
 
 
 def get_asterisk(password):
+    """Get the number of asterisks"""
     length = len(password)
     print(f"{length * "*"}")
 
 
 def get_password():
+    """Check for valid password"""
     password = input("Enter password: ")
     while len(password) <= PASSWORD_LENGTH:
         print("Invalid Password")
