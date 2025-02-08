70312d08db431037e6a827572904a59dfadaa514
kierananthiniabraham <kieran.abraham@my.jcu.edu.au>
2025-02-08 19:33:26 +0800

    Done with refactoring score file

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,18 +2,30 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 LOWEST_SCORE = 0
 HIGHEST_SCORE = 100
 EXCELLENT = 90
 PASSABLE = 50
 
-score = float(input("Enter score: "))
-if score < LOWEST_SCORE or score > HIGHEST_SCORE:
-    print("Invalid score")
-elif score >= EXCELLENT:
-    print("Excellent")
-elif score >= PASSABLE:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(get_result(score))
+
+    random_score = random.randint(LOWEST_SCORE,HIGHEST_SCORE)
+    print(random_score)
+    print(get_result(random_score))
+
+def get_result(score):
+    if score < LOWEST_SCORE or score > HIGHEST_SCORE:
+        return "Invalid score"
+    elif score >= EXCELLENT:
+        return "Excellent"
+    elif score >= PASSABLE:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
