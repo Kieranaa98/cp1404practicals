93f9c2d49b99eaad5d8fc823a17b183d5b7aef2d
kierananthiniabraham <kieran.abraham@my.jcu.edu.au>
2025-02-08 19:08:29 +0800

    refactored the get password function

--- prac_02/passwords_stars.py
+++ prac_02/passwords_stars.py
@@ -1,13 +1,19 @@
 PASSWORD_LENGTH =10
 
 def main():
-    password = input("Enter password: ")
-    while len(password) <= PASSWORD_LENGTH:
-        print("Invalid Password")
-        password = input("Enter password: ")
+    password = get_password()
 
     length = len(password)
 
     print(f"{length * "*"}")
+
+
+def get_password():
+    password = input("Enter password: ")
+    while len(password) <= PASSWORD_LENGTH:
+        print("Invalid Password")
+        password = input("Enter password: ")
+    return password
+
 
 main()
