97644735cc6f0f8789342d10aeea9b85bf2a85b9
kierananthiniabraham <kieran.abraham@my.jcu.edu.au>
2025-02-08 22:47:05 +0800

    Edited get result function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -11,13 +11,12 @@ def main():
     """Main function"""
     print(MENU)
     choice = input(">>> ").upper()
-    score = int(input("Enter score: "))
+    score = get_valid_score()
 
     while choice != "Q":
         if choice == "G":
-            get_valid_score(score)
+            score = get_valid_score()
         elif choice == "P":
-            get_result(score)
             print(get_result(score))
         elif choice == "S":
             get_stars(score)
@@ -27,10 +26,13 @@ def main():
         choice = input(">>> ").upper()
     print("Thank you.")
 
-def get_valid_score(score):
-    """Get valid score"""
-    if score < LOWEST_SCORE or score > HIGHEST_SCORE:
-        return "Invalid score"
+def get_valid_score():
+    """Get a valid score from user input"""
+    score = int(input("Enter score: "))
+    while score < LOWEST_SCORE or score > HIGHEST_SCORE:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    return score
 
 def get_result(score):
     """Determine the result of the score"""
@@ -43,7 +45,6 @@ def get_result(score):
 
 def get_stars(score):
     """Print the number of asterisks according to score"""
-    print(f"{score * "*"}")
-
+    print("*" * score)
 
 main()
