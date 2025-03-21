839488560885ef66ad06ef133976b9da8dae1de5
kierananthiniabraham <kieran.abraham@my.jcu.edu.au>
2025-03-21 17:42:29 +0800

    Adjusted the guitar file

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,7 +1,6 @@
 CURRENT_YEAR = 2025
 VINTAGE_AGE = 50
 
-
 class Guitar:
     """Guitar class for storing details of a guitar."""
 
@@ -22,3 +21,7 @@ class Guitar:
     def is_vintage(self):
         """Determine if a Guitar is considered vintage or not based on age."""
         return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        """Compare guitars by year."""
+        return self.year < other.year
