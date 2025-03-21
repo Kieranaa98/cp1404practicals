d12c43771f938274369aa18f3a2e9b51080577ca
kierananthiniabraham <kieran.abraham@my.jcu.edu.au>
2025-03-22 00:52:51 +0800

    Made some changes

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -3,11 +3,6 @@ CP1404/CP5632 Practical - Suggested Solution
 Programming Language class with tests.
 """
 
-"""
-CP1404/CP5632 Practical - Suggested Solution
-Programming Language class with tests.
-"""
-
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
